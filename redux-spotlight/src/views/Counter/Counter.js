31760b851fc7d953b2357a4aeb6125bee3220934
ajoy267 <andrew.joy12@hotmail.com>
2022-02-15 11:48:39 -0800

    cant figure out why my dark mode toggle isnt working, I've used this code before and never had this issue

--- redux-spotlight/src/views/Counter/Counter.js
+++ redux-spotlight/src/views/Counter/Counter.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import DarkModeToggle from '../../hooks/DarkMode';
 import { decrement, increment } from './counterSlice';
 
 const Counter = () => {
-  // The function below is called a selector and allows us to select a value from
+  // The function below is called a selector and allows us 2 select a value from
   // the state.
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
     <div>
-      <button
-        aria-label="Decrement value"
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      >
-        -
-      </button>
-      <span>{count}</span>
-      <button
-        aria-label="Increment value"
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        +
-      </button>
+      <div>
+        <button
+          aria-label="Decrement value"
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          -
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Increment value"
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          +
+        </button>
+      </div>
+      <DarkModeToggle />
     </div>
   );
 };
